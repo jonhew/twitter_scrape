0433272c77a91f6c43ed3a3e9c2395f1955b3f86
asdasdasdasdasd123 <wawagto6@gmail.com>
2014-07-19 17:42:36 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -28,25 +28,30 @@
 
 import scraperwiki
 import simplejson
-import urllib2
 
-# Get results from the Twitter API! Change QUERY to your search term of choice. 
-# Examples: 'PCC "Police Commissioner"
-QUERY = 'Police Commissioner'
-RESULTS_PER_PAGE = '100'
-LANGUAGE = 'eng'
-NUM_PAGES = 20 
+# retrieve a page
+base_url = 'http://search.twitter.com/search.json?q='
+q = '#ConMaduroAlVolante'
+options = '&rpp=600&page='
+page = 1
 
-for page in range(1, NUM_PAGES+1):
-    base_url = 'http://search.twitter.com/search.json?q=%s&rpp=%s' \
-         % (urllib2.quote(QUERY), RESULTS_PER_PAGE)
+while 1:
     try:
-        results_json = simplejson.loads(scraperwiki.scrape(base_url))
-        for result in results_json['results']:
+        url = base_url + q + options + str(page)
+        html = scraperwiki.scrape(url)
+        #print html
+        soup = simplejson.loads(html)
+        for result in soup['results']:
             data = {}
             data['id'] = result['id']
             data['text'] = result['text']
             data['from_user'] = result['from_user']
-            print data['from_user'], data['text']
+            data['created_at']= result['created_at']   
+
+
+            # save records to the datastore
+            scraperwiki.sqlite.save(["id"], data) 
+        page = page + 1
     except:
-        pass
+        print str(page) + ' pages scraped'
+        break
