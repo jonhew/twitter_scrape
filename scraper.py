c66632d33c3e813bf953b7215ee13038c45714b0
asdasdasdasdasd123 <wawagto6@gmail.com>
2014-07-19 17:38:03 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -22,35 +22,31 @@
 # is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 # has at least a table called data.
 
+###################################################################################
+# Twitter API scraper - designed to be forked and used for more interesting things
+###################################################################################
+
 import scraperwiki
 import simplejson
 import urllib2
-import urllib
 
-# Get results from the Twitter API! 
-
-search_term = 'Europe'
-geocode = '37.781157,-122.398720,25mi'
-date = '2013-05-10'
-language = 'en'
-
-NUM_PAGES = 2
-RESULTS_PER_PAGE = '10'
-
-basic_url = 'https://search.twitter.com/search.json?'
-query_dict = {'q':search_term, 'lang':language, 'geocode':geocode, 'until':date}
-query = urllib.urlencode(query_dict)
+# Get results from the Twitter API! Change QUERY to your search term of choice. 
+# Examples: 'PCC "Police Commissioner"
+QUERY = 'Police Commissioner'
+RESULTS_PER_PAGE = '100'
+LANGUAGE = 'eng'
+NUM_PAGES = 20 
 
 for page in range(1, NUM_PAGES+1):
-
-        fh = urllib.urlopen(basic_url + query)
-        response = fh.read()        
-        result_json = simplejson.loads(response)
-        # print result_json
-        #print result_json['results']
-        for result in result_json['results']:
-            #print result
-            result['geocode'] = geocode
-            scraperwiki.sqlite.save(unique_keys=['id'], data=result, table_name="Tweets")
-        
-    
+    base_url = 'http://search.twitter.com/search.json?q=%s&rpp=%s' \
+         % (urllib2.quote(QUERY), RESULTS_PER_PAGE)
+    try:
+        results_json = simplejson.loads(scraperwiki.scrape(base_url))
+        for result in results_json['results']:
+            data = {}
+            data['id'] = result['id']
+            data['text'] = result['text']
+            data['from_user'] = result['from_user']
+            print data['from_user'], data['text']
+    except:
+        pass
